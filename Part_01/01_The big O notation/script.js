462e8c2d375a2acb5d34fe022782427d50b2e428
EverLookNeverSee <EverLookNeverSee@Proton.Me>
2023-02-20 09:03:45 +0330

    Adding examples (2 - 5)

--- Part_01/01_The big O notation/script.js
+++ Part_01/01_The big O notation/script.js
@@ -1,1 +1,50 @@
 "use strict";
+
+// We use the big O notation to describe the performance of an algorithm.
+/*
+* The big O notation according to wikipedia:
+* It is a mathematical notation that describes the limiting behavior of a function when the
+* argument tends towards a particular value or infinity.
+* */
+
+// Example 1:
+const arr1 = [1, 2, 3, 4, 5, 6, 7];
+function f1(array) {
+  console.log(array[0]);  // Runtime complexity: O(1)
+}
+f1(arr1);
+
+
+// Example 2:
+const arr2 = [8, 9, 10, 11, 12];
+function f2(array) {
+  console.log(array[0]);   // Runtime complexity: O(1)
+  console.log(array[1]);   // Runtime complexity: O(1)
+}
+f2(arr2); // Overall runtime complexity: O(1)
+
+
+// Example 3:
+const arr3 = [1, 2, 3, 4, 5, 6];
+function f3(array) {
+  console.log(array[0]); // O(1)
+  for (let i = 0; i < array.length; i++) {
+    console.log(array[i]); // O(n)
+  }
+  console.log(array[1]);  // O(1)
+}
+f3(arr3);
+// Overall runtime complexity: O(n)
+
+
+// Example 4:
+function f4(a, b) {
+  for (let i = 0; i < a.length; i++) {
+    console.log(a[i]); // O(n)
+  }
+  for (let j = 0; j < b.length; j++) {
+    console.log(b[j]); // O(m)
+  }
+}
+f4(arr3, arr1);
+// Overall runtime complexity: O(n)
