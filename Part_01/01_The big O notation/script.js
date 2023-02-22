dee016010b73797ca8ac74b57c5628628303eb1e
EverLookNeverSee <EverLookNeverSee@Proton.Me>
2023-02-22 18:18:38 +0330

    Adding examples 5 and 6

--- Part_01/01_The big O notation/script.js
+++ Part_01/01_The big O notation/script.js
@@ -48,3 +48,30 @@ function f4(a, b) {
 }
 f4(arr3, arr1);
 // Overall runtime complexity: O(n)
+
+
+// Example 5:
+function f5(array) {
+  for (let i = 0; i < array.length; i++) {  // O(n)
+    for (let j = 0; j < array.length; j++) {  // O(n)
+      console.log(`(${i}, ${j})`);
+    }
+  }
+}
+f5(arr1);
+// Overall runtime complexity: O(n * n) = O(n^2) Quadratic
+
+
+// Example 6:
+function f6(array) {
+  for (let k = 0; k < array.length; k++) {  // O(n)
+    console.log(`k is: ${k}`);
+  }
+  for (let i = 0; i < array.length; i++) {  // O(n)
+    for (let j = 0; j < array.length; j++) {  // O(n)
+      console.log(`(${i}, ${j})`);
+    }
+  }
+}
+f6(arr1);
+// Overall runtime complexity: O(n + n ^ 2) = O(n ^ 2)
