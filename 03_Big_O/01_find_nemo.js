246ac547c280630e30abc12c5e6e51a7c9b2d90d
EverLookNeverSee <EverLookNeverSee@Proton.Me>
2023-03-06 22:03:07 +0330

    Finishing lecture 28

--- 03_Big_O/01_find_nemo.js
+++ 03_Big_O/01_find_nemo.js
@@ -5,15 +5,12 @@ const nemo = ["nemo"];
 const large = new Array(10000).fill("nemo");
 
 function findNemo(array) {
-  const t0 = performance.now();
   for (let i = 0; i < array.length; i++) {
     if (array[i] === "nemo") {
       console.log("Found Nemo!");
     }
   }
-  const t1 = performance.now();
-  console.log(`findNemo took ${t1 - t0} milliseconds.`);
 }
 
-findNemo(large);
+findNemo(large);  // O(n) --> Linear time
 ////////////////////////////////////////////////////////////////////////////////////
