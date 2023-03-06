8cb4908ddc6e2a7fb7beb751d986ac6551d9f29a
EverLookNeverSee <EverLookNeverSee@Proton.Me>
2023-03-06 22:32:48 +0330

    Finishing lecture 33

--- 03_Big_O/01_find_nemo.js
+++ 03_Big_O/01_find_nemo.js
@@ -37,3 +37,22 @@ function funChallenge(input) {
 
 funChallenge([1, 2, 3]);  // O(3 + 4n) = O(n)
 ////////////////////////////////////////////////////////////////////////////////////
+function anotherFunChallenge(input) {
+  let a = 5;  // O(1)
+  let b = 10; // O(1)
+  let c = 50; // O(1)
+  for (let i = 0; i < input; i++) {
+    let x = i + 1;  // O(n)
+    let y = i + 2;  // O(n)
+    let z = i + 3;  // O(n)
+
+  }
+  for (let j = 0; j < input; j++) {
+    let p = j * 2;  // O(n)
+    let q = j * 2;  // O(n)
+  }
+  let whoAmI = "I don't know";  // O(1)
+}
+
+anotherFunChallenge([1, 2, 3]); // O(4 + 5n) = O(n)
+////////////////////////////////////////////////////////////////////////////////////
