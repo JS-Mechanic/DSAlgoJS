fde8858937027caac5cd0398176694f8c2210965
EverLookNeverSee <EverLookNeverSee@Proton.Me>
2023-03-06 22:12:38 +0330

    Finishing lecture 29

--- 03_Big_O/01_find_nemo.js
+++ 03_Big_O/01_find_nemo.js
@@ -14,3 +14,12 @@ function findNemo(array) {
 
 findNemo(large);  // O(n) --> Linear time
 ////////////////////////////////////////////////////////////////////////////////////
+const boxes = [0, 1, 2, 3, 4, 5, 6];
+
+function logFirstTwoBoxes(boxes) {
+  console.log(boxes[0]);  // O(1)
+  console.log(boxes[1]);  // O(1)
+}
+
+logFirstTwoBoxes(boxes);  // O(2) = O(1)
+////////////////////////////////////////////////////////////////////////////////////
