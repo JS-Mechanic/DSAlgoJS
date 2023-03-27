3fdd0e61e0572064f92b0a7529e419cf50105460
Milad Sadeghi DM <EverLookNeverSee@Proton.Me>
2023-03-27 19:42:12 +0330

    Defining an array and adding some other descriptions

--- 06_Data_Structures_Arrays/main.js
+++ 06_Data_Structures_Arrays/main.js
@@ -7,3 +7,14 @@
 *     O(1)          O(1)|O(n)   O(n)          O(n)
 *
 * */
+
+// Defining an array:
+const stringsArray = ["a", "b", "c", "d"];
+
+/*
+*
+* In 64-bit operating systems:
+* 8 byte will be used to store each element, So:
+* 8 times 4 = 32 bytes will be used for array above
+*
+* */
