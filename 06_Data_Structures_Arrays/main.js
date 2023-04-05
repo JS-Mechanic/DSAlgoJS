e424e91f75aa729e5c956583e9c091a767c52e23
Milad Sadeghi DM <EverLookNeverSee@Proton.Me>
2023-04-05 18:57:27 +0330

    Merge sorted arrays

--- 06_Data_Structures_Arrays/main.js
+++ 06_Data_Structures_Arrays/main.js
@@ -105,3 +105,34 @@ console.log(reverseStrV5("Milad Sadeghi DM"));
 
 console.log("-".repeat(50));
 ////////////////////////////////////////////////////////////////////////////////////
+// Merge sorted arrays:
+const arr1 = [0, 3, 4, 31];
+const arr2 = [4, 6, 30, 36, 48];
+
+function mergeSortedArrays(array1, array2) {
+  const mergedArray = [];
+  if (array1.length === 0) return array2;
+  if (array2.length === 0) return array1;
+  let array1Item = array1[0];
+  let array2Item = array2[0];
+  let i = 1;
+  let j = 1;
+  while (array1Item || array2Item) {
+    console.log(array1Item, array2Item);
+    if (!array2Item || array1Item < array2Item) {
+      mergedArray.push(array1Item);
+      array1Item = array1[i];
+      i++;
+    } else {
+      mergedArray.push(array2Item);
+      array2Item = array2[j];
+      j++;
+    }
+  }
+  return mergedArray;
+}
+
+const result = mergeSortedArrays(arr1, arr2);
+console.log(result);
+
+////////////////////////////////////////////////////////////////////////////////////
