f58440c97a8ab59fe7557cb31692f04b8fad2259
Milad Sadeghi DM <EverLookNeverSee@Proton.Me>
2023-04-07 12:07:49 +0330

    Implementing a hashtable from scratch

--- 07_Data_Structures_Hash_Tables/main.js
+++ 07_Data_Structures_Hash_Tables/main.js
@@ -17,3 +17,47 @@ console.log(user);
 user.scream();  // O(1)
 
 ////////////////////////////////////////////////////////////////////////////////////
+// Implementing a hashtable from scratch:
+class HashTable {
+	constructor(size) {
+		this.data = new Array(size);
+	}
+	
+	#hash(key) {    // Despite there is a for loop, but hash function is very fast, So O(1)
+		let hash = 0;
+		for (let i = 0; i < key.length; i++) {
+			hash = (hash + key.charCodeAt(i) * i) % this.data.length;
+		}
+		return hash;
+	}
+	
+	set(key, value) {   // O(1)
+		let address = this.#hash(key);
+		if (!this.data[address]) {
+			this.data[address] = [];
+		}
+		this.data[address].push([key, value]);
+		return this.data;
+	}
+	
+	get(key) {  // Most of the time, O(1) - In a very bad situation such as memory size of 2, O(n)
+		let address = this.#hash(key);
+		const currentBucket = this.data[address];
+		if (currentBucket) {
+			for (let i = 0; i < currentBucket.length; i++) {
+				if (currentBucket[i][0] === key) {
+					return currentBucket[i][1];
+				}
+			}
+		}
+		return undefined;
+	}
+}
+
+
+const ht = new HashTable(50);
+
+ht.set("grapes", 1000);
+console.log(ht.get("grapes"));
+
+////////////////////////////////////////////////////////////////////////////////////
