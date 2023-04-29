4ca72f456133e9990cd2c909658036a1dad54e84
Milad Sadeghi DM <EverLookNeverSee@Proton.Me>
2023-04-30 00:12:25 +0330

    Implementing DoublyNode and DoublyLinkedList class

--- 08_Data_Structures_Linked_Lists/main.js
+++ 08_Data_Structures_Linked_Lists/main.js
@@ -103,3 +103,99 @@ ll.append(20);
 ll.insert(2, 12);
 ll.remove(1);
 console.log(ll.print());
+
+////////////////////////////////////////////////////////////////////////////////////
+// Implementing a doubly linked list from scratch:
+class DoublyNode {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+        this.prev = null;
+    }
+}
+
+
+class DoublyLinkedList {
+    constructor(value) {
+        this.head = new DoublyNode(value);
+        this.tail = this.head;
+        this.length = 1;
+    }
+
+    append(value) {
+        const newNode = new DoublyNode(value);
+        newNode.prev = this.tail;
+        this.tail.next = newNode;
+        this.tail = newNode;
+        this.length++;
+        return this;
+    }
+
+    prepend(value) {
+        const newNode = new DoublyNode(value);
+        newNode.next = this.head;
+        this.head.prev = newNode;
+        this.head = newNode;
+        this.length++;
+        return this;
+    }
+
+    print() {
+        const array = [];
+        let currentNode = this.head;
+        while (currentNode !== null) {
+            array.push(currentNode.value);
+            currentNode = currentNode.next;
+        }
+        return array;
+    }
+
+    insert(index, value) {
+        if (index >= this.length) return this.append(value);
+        const newNode = new DoublyNode(value);
+        const leader = this.traverseToIndex(index - 1);
+        const follower = leader.next;
+        leader.next = newNode;
+        newNode.prev = leader;
+        newNode.next = follower;
+        follower.prev = newNode;
+        this.length++;
+        return this;
+    }
+
+    remove(index) {
+        if (this.length === 1) { // There is only one node inside the linked list.
+            this.head = null;
+            this.tail = null;
+            this.length--;
+            return this;
+        } else if (index === 0) {   // Removing first node.
+            this.head = this.traverseToIndex(index + 1);
+            this.head.prev = null;
+            this.length--;
+            return this;
+        } else if (index === this.length - 1) { // Removing last node.
+            this.tail = this.traverseToIndex(index - 1);
+            this.tail.next = null;
+            this.length--;
+            return this;
+        } else {    // Removing node somewhere in the middle of linked list.
+            const nodeBefore = this.traverseToIndex(index - 1);
+            const nodeAfter = this.traverseToIndex(index + 1);
+            nodeBefore.next = nodeAfter;
+            nodeAfter.prev = nodeBefore;
+            this.length--;
+            return this;
+        }
+    }
+
+    traverseToIndex(index) {
+        let counter = 0;
+        let currentNode = this.head;
+        while (counter !== index) {
+            currentNode = currentNode.next;
+            counter++;
+        }
+        return currentNode;
+    }
+}
