4a0bac831f964f6aae46bb2ac4a860d62f316ceb
Milad Sadeghi DM <EverLookNeverSee@Proton.Me>
2023-05-07 14:21:14 +0330

    Implementing Stack class

--- 09_Data_Structures_Stacks_Queues/main.js
+++ 09_Data_Structures_Stacks_Queues/main.js
@@ -6,3 +6,45 @@ class Node {
         this.next = null;
     }
 }
+
+class Stack {
+    constructor() {
+        this.top = null;
+        this.bottom = null;
+        this.length = 0;
+    }
+
+    peak() {
+        // See the top element
+        return this.top;
+    }
+
+    push(value) {
+        // Push a value to the stack
+        const newNode = new Node(value);
+        if (this.length === 0) {
+            this.bottom = newNode;
+            this.top = newNode;
+        } else {
+            const pointerHolder = this.top;
+            this.top = newNode;
+            this.top.next = pointerHolder;
+        }
+        this.length++;
+        return this;
+    }
+
+    pop() {
+        // Remove a value from on top of stack
+        if (!this.top) return null;
+        if (this.top === this.bottom) this.bottom = null;
+        this.top = this.top.next;
+        this.length--;
+        return this;
+    }
+
+    isEmpty() {
+        // whether the stack is empty ir not
+        return this.length === 0;
+    }
+}
