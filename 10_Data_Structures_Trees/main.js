4fa411e608e9fb9aa21486fa3deff748332f0f0d
Milad Sadeghi DM <EverLookNeverSee@Proton.Me>
2023-05-21 21:16:28 +0330

    Implementing BinarySearchTree class and its insert method

--- 10_Data_Structures_Trees/main.js
+++ 10_Data_Structures_Trees/main.js
@@ -8,3 +8,35 @@ class Node {
         this.value = value;
     }
 }
+
+class BinarySearchTree {
+    constructor() {
+        this.root = null;
+    }
+    insert(value) {
+        const newNode = new Node(value);
+        if (this.root === null) this.root = newNode;
+        else {
+            let currentNode = this.root;
+            while (true) {
+                if (value < currentNode.value) {
+                    // Go to the left side
+                    if (!currentNode.left) {
+                        currentNode.left = newNode;
+                        return this;
+                    } else {
+                        currentNode = currentNode.left;
+                    }
+                } else {
+                    // Go to the right side
+                    if (!currentNode.right) {
+                        currentNode.right = newNode;
+                        return this;
+                    } else {
+                        currentNode = currentNode.right;
+                    }
+                }
+            }
+        }
+    }
+}
