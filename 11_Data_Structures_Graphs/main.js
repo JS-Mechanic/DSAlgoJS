29ab8dc3428d86d78bb9b281fd2eeaae8978a497
Milad Sadeghi DM <EverLookNeverSee@Proton.Me>
2023-06-12 11:41:58 +0330

    Testing graph data structure functionality

--- 11_Data_Structures_Graphs/main.js
+++ 11_Data_Structures_Graphs/main.js
@@ -54,3 +54,41 @@ class Graph {
         }
     }
 }
+
+
+/*
+
+    3 ---------- 4 ---------- 5
+    |            |            |
+    |            |            |
+    |            |            |
+    1 ---------- 2            6
+    \           /
+     \         /
+      \       /
+       \     /
+        \   /
+          0
+
+ */
+
+const myGraph = new Graph();
+
+myGraph.addVertex("0");
+myGraph.addVertex("1");
+myGraph.addVertex("2");
+myGraph.addVertex("3");
+myGraph.addVertex("4");
+myGraph.addVertex("5");
+myGraph.addVertex("6");
+
+myGraph.addEdge("0", "1");
+myGraph.addEdge("0", "2");
+myGraph.addEdge("1", "2");
+myGraph.addEdge("1", "3");
+myGraph.addEdge("2", "4");
+myGraph.addEdge("3", "4");
+myGraph.addEdge("4", "5");
+myGraph.addEdge("5", "6");
+
+myGraph.showConnections();
