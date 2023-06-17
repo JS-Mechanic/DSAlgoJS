e36d91916e78caf56cf60fc7746b3efe1e1438bd
Milad Sadeghi DM <EverLookNeverSee@Proton.Me>
2023-06-17 18:12:10 +0330

    Implementing factorial function using two approaches

--- 12_Algorithms_Recursion/main.js
+++ 12_Algorithms_Recursion/main.js
@@ -5,4 +5,26 @@ function callItself() {
     callItself();
 }
 
-callItself();   // RangeError: Maximum call stack size exceeded
+// callItself();   // RangeError: Maximum call stack size exceeded
+
+
+/*
+Write two function that find the factorial of any number. One should use recursive
+and the other should just use a for loop.
+ */
+
+const findFactorialIterative = function (num) { // O(n)
+    let accumulator = 1;
+    for (let i = 2; i <= num; i++) accumulator *= i;
+    return accumulator;
+}
+
+console.log(`Iterative: ${findFactorialIterative(5)}`);
+
+
+const findFactorialRecursive = function (num) { // O(n)
+    if (num === 2) return 2;
+    return num * findFactorialRecursive(num - 1);
+}
+
+console.log(`Recursive: ${findFactorialRecursive(5)}`);
