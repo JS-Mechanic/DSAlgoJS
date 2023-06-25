951271c3dc3df8a714aabda9e674900ad718ad76
Milad Sadeghi DM <EverLookNeverSee@Proton.Me>
2023-06-25 18:16:58 +0330

    Experimenting sort method in js

--- 13_Algorithms_Sorting/main.js
+++ 13_Algorithms_Sorting/main.js
@@ -1,1 +1,8 @@
 "use strict";
+
+const letters = ["a", "d", "z", "e", "r", "b"];
+const basket = [2, 65, 34, 2, 1, 7, 8];
+console.log(letters.sort());
+console.log(basket.sort());
+console.log(basket.sort((a, b) => a - b));
+console.log(basket.sort((a, b) => b - a));
