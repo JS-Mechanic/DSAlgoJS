71f15c01ba4bfb3f6b0cd43aad32fffbbb1245cf
Milad Sadeghi DM <miladsadeghidm@gmail.com>
2023-07-25 13:47:49 +0330

    Implementing quickSort function

--- 13_Algorithms_Sorting/main.js
+++ 13_Algorithms_Sorting/main.js
@@ -99,3 +99,20 @@ function merge(left, right) {
 }
 
 console.log(mergeSort(numbers));
+
+
+function quickSort(array) {
+	if (array.length <= 1) return array;
+	let pivot = array[0];
+	let left = [];
+	let right = [];
+	for (let i = 1; i < array.length; i++) {
+		array[i] < pivot ? left.push(array[i]) : right.push(array[i]);
+	}
+
+	return quickSort(left).concat(pivot, quickSort(right));
+}
+
+numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+const qsResult = quickSort(numbers);
+console.log(qsResult);
