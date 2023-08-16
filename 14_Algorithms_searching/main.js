358960a70156ac828f78372a30fb4affe9e02ace
Milad Sadeghi DM <miladsadeghidm@gmail.com>
2023-08-16 20:38:15 +0330

    Implementing BFS search method and testing its functionality

--- 14_Algorithms_searching/main.js
+++ 14_Algorithms_searching/main.js
@@ -68,4 +68,29 @@ class BinarySearchTree {
         }
         return false;
     }
+
+    breadthFirstSearch() {
+        let currentNode = this.root;
+        let list = [];
+        let queue = [];
+        queue.push(currentNode);
+        while (queue.length > 0) {
+            currentNode = queue.shift();
+            list.push(currentNode.value);
+            if (currentNode.left) queue.push(currentNode.left);
+            if (currentNode.right) queue.push(currentNode.right);
+        }
+        return list;
+    }
 }
+
+
+const tree = new BinarySearchTree();
+tree.insert(9);
+tree.insert(4);
+tree.insert(6);
+tree.insert(20);
+tree.insert(170);
+tree.insert(15);
+tree.insert(1);
+console.log(tree.breadthFirstSearch());
