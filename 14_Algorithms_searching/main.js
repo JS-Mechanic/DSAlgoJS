cc9ba29f64234182ce0aa3a89cab9afeff79e466
Milad Sadeghi DM <miladsadeghidm@gmail.com>
2023-08-16 20:37:11 +0330

    Reimplementing binary search tree data structure

--- 14_Algorithms_searching/main.js
+++ 14_Algorithms_searching/main.js
@@ -13,3 +13,59 @@ console.log(fruits.find((item) => item === "Mango"));
 
 // Fourth way:
 console.log(fruits.includes("Mango"));
+
+console.log("*".repeat(30));
+
+// Breadth First Search in JavaScript:
+// Binary Search Tree:
+class Node {
+    constructor(value) {
+        this.right = null;
+        this.left = null;
+        this.value = value;
+    }
+}
+
+class BinarySearchTree {
+    constructor() {
+        this.root = null;
+    }
+
+    insert(value) {
+        const newNode = new Node(value);
+        if (this.root === null) this.root = newNode;
+        else {
+            let currentNode = this.root;
+            while (true) {
+                if (value < currentNode.value) {
+                    // Go to the left side
+                    if (!currentNode.left) {
+                        currentNode.left = newNode;
+                        return this;
+                    } else {
+                        currentNode = currentNode.left;
+                    }
+                } else {
+                    // Go to the right side
+                    if (!currentNode.right) {
+                        currentNode.right = newNode;
+                        return this;
+                    } else {
+                        currentNode = currentNode.right;
+                    }
+                }
+            }
+        }
+    }
+
+    lookup(value) {
+        if (!this.root) return false;
+        let currentNode = this.root;
+        while (currentNode) {
+            if (value < currentNode.value) currentNode = currentNode.left;
+            else if (value > currentNode.value) currentNode = currentNode.right;
+            else return currentNode;
+        }
+        return false;
+    }
+}
